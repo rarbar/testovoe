e21a510cee24e5c48a6bdd560ff14406cb4bbea3
rarbar <senloren@yandex.ru>
2021-11-28 13:33:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './components/Home/Home.module.css';
+import {Route, Routes} from 'react-router-dom';
+import {Home} from './components/Home/Home';
+import {News} from './components/Card/News';
+import {fetchCardsTC} from './store/appReducer';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from './store/store';
+import {cardType} from './api/cardsApi';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function AppRoute() {
+    const cards = useSelector<AppRootStateType, Array<cardType>>((state) => state.cards.cards)
+    const isLoading = useSelector<AppRootStateType, boolean>((state) => state.cards.isLoading)
+    const [language, setLanguage] = useState({value: '1', label: 'RU'})
+    const dispatch = useDispatch()
+    const mainLangId = '1'
+    const countPage = '9'
+    useEffect(() => {
+        dispatch(fetchCardsTC(countPage, mainLangId))
+    }, [])
+    return (<>
+            <Routes>
+                <Route path="/"
+                       element={
+                           <Home cards={cards}
+                                 language={language}
+                                 countPage={countPage}
+                                 isLoading={isLoading}
+                                 setLanguage={setLanguage}/>
+                       }/>
+                <Route path="/:id"
+                       element={<News cards={cards}
+                                      isLoading={isLoading}/>}
+                />
+            </Routes>
+        </>
+    );
 }
 
-export default App;
+export default AppRoute;
